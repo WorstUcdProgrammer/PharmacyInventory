e30aae4152b2d2c162f03147b831aa0d1ff1a545
WorstUcdProgrammer <yizzhou@ucdavis.edu>
2023-07-07 19:46:23 -0400

    routes configured

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
+// connect to database
 connectDB();
 
 const app = express();
@@ -10,4 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
+// routes
+app.use('/drug', require('./routes/drugRoutes'));
+app.use('/history', require('./routes/historyRoutes'));
+app.use('/sale', require('./routes/saleRoutes'));
+
+// connect to port
 app.listen(port, () => console.log(`Server started on port ${port}`));
