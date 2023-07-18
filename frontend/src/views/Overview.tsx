e8b0ab152bd6872c25d131fcf64512790188e3a1
WorstUcdProgrammer <yizzhou@ucdavis.edu>
2023-07-17 20:47:30 -0400

    Promise.all multiple fetch

--- frontend/src/views/Overview.tsx
+++ frontend/src/views/Overview.tsx
@@ -16,24 +16,41 @@ interface InventoryItem {
   price: number;
 }
 
+interface RecordItem {
+  _id: string;
+  drug: string;
+  type: string;
+  mgPerUnit: number;
+  cost: number;
+  price: number;
+  quantity: number;
+  time: string;
+}
+
 const Overview: React.FC = () => {
   const [items, setItems] = useState<InventoryItem[]>([]);
+  const [histories, setHistories] = useState<RecordItem[]>([]);
   useEffect(() => {
-    const fetchInventoryItems = async () => {
+    const fetchItems = async () => {
       try {
-        const response = await fetch("http://localhost:5000/drug");
-        const data = await response.json();
-        setItems(data);
+        const [responseItem, responseHistory] = await Promise.all([
+          fetch("http://localhost:5000/drug"),
+          fetch("http://localhost:5000/history"),
+        ]);
+        const dataItem = await responseItem.json();
+        const dateHistory = await responseHistory.json();
+        setItems(dataItem);
       } catch (error) {
-        console.error("Error fetching inventory items:", error);
+        console.error("Error fetching items:", error);
       }
     };
-    fetchInventoryItems();
+    fetchItems();
   }, []);
   return (
     <div>
       <div className="flex justify-left mt-8">
         <div className="w-1/4">
+          <h2 className="text-center">Low Stock Medication</h2>
           <MedicationAvailability medications={items} />
         </div>
       </div>
